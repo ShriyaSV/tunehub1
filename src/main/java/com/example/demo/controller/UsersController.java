dcc09e9f010d58bd5f1833f177e2238cfd21aa3f
ShriyaSV <shriyashriya1976@gmail.com>
2024-01-12 16:49:02 +0530

    1st version of tunehub

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +39,7 @@ public class UsersController
 	@PostMapping("/validate")
 	public String validate(@RequestParam("email")String email,
 			@RequestParam("password") String password,
-			HttpSession session) {
+			HttpSession session, Model model) {
 		
 		if(service.validateUser(email,password) == true) {
 			String role = service.getRole(email);
@@ -49,6 +50,10 @@ public class UsersController
 				return "adminHome";
 			}
 			else {
+				Users user = service.getUser(email);
+				boolean userStatus = user.isPremium();
+				model.addAttribute("isPremium", userStatus);
+				
 				return "customerHome";
 			}
 		}
